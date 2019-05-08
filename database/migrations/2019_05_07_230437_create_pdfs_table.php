74be00991fff4332e6b1d46e3a42833009e390fc
Fabian Cudis <fabclub747@gmail.com>
2019-05-07 20:52:15 -0400

    Pdf migration

--- database/migrations/2019_05_07_230437_create_pdfs_table.php
+++ database/migrations/2019_05_07_230437_create_pdfs_table.php
@@ -16,6 +16,24 @@ class CreatePdfsTable extends Migration
         Schema::create('pdfs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->char('client_name', 20);
+            $table->date('date');
+            $table->char('address', 100);
+            $table->char('office_number', 4);
+            $table->char('plz_number', 4);
+            $table->integer('price');
+            $table->integer('regular_price');
+            $table->boolean('bath');
+            $table->boolean('park');
+            $table->boolean('wifi');
+            $table->boolean('dinning_room');
+            $table->integer('surface');
+            $table->integer('capacity');
+            $table->char('agent_name', 20);
+            $table->char('agent_email', 20);
+            $table->char('agent_phone', 20);
+
+
         });
     }
 
